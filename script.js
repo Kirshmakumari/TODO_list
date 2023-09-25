2eef3f52c72a7cb2b2e534dadba94c6c343ae59e
Kirshmakumari <kumarikirshma@gmail.com>
2023-09-26 00:15:06 +0500

    todo-list

---
+++ script.js
@@ -0,0 +1,49 @@
+// *****add btn*****
+function addItem() {
+    var input = document.getElementById("todoInput");
+    var list = document.getElementById("list");
+    var li = document.createElement("li");
+    var liText = document.createTextNode(input.value);
+    li.setAttribute("class","listItem")
+    li.appendChild(liText);
+    list.appendChild(li);
+    console.log(li);
+    document.getElementById("todoInput").value = "";
+    
+    // *****Delete btn*****
+    var delBtn = document.createElement("button");
+    var delBtnText = document.createTextNode("Delete");
+    delBtn.setAttribute("onclick", "delItem(this)");
+    delBtn.appendChild(delBtnText);
+    li.appendChild(delBtn);
+    // *****Edit btn*****
+    var editBtn = document.createElement("button");
+    var editBtnText = document.createTextNode("Edit");
+    editBtn.setAttribute("onclick", "editItem(this)");
+    editBtn.appendChild(editBtnText);
+    li.appendChild(editBtn);
+}
+
+// *****delete all btn*****
+function deleteAll() {
+    var list = document.getElementById("list");
+    list.remove();
+    //list.innerHTML="";     *****"another way to remove"*****
+}
+
+// *****delItem Function onclick Delete btn*****
+function delItem(currentItem) {
+    currentItem.parentNode.remove();
+    console.log(currentItem.parentNode, "is deleted by you");
+}
+
+// *****editItem Function onclick Edit btn*****
+function editItem(currentLi) {
+    var priviousValue = currentLi.parentNode.firstChild.nodeValue;
+    var inputUpdatedValue = prompt("Enter Updated Value", priviousValue);
+    currentLi.parentNode.firstChild.nodeValue = inputUpdatedValue;
+    console.log(
+        "priviousValue = " + priviousValue,
+        "| UpdatedValue = " + inputUpdatedValue
+    );
+}
